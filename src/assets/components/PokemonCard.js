ac9038b62c4249d19bc60081dec40e23f2d870a4
Maicol Ortiz Hernandez <107804493+maicolortz@users.noreply.github.com>
2023-03-17 17:05:38 -0500

    style: style in card Pokemon

--- src/assets/components/PokemonCard.js
+++ src/assets/components/PokemonCard.js
@@ -6,9 +6,13 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import React from "react";
-
+import getColorByPokemonType from "../../utils/getColorByPokemonType";
+import { capitalize } from "lodash";
 export default function PokemonCard(props) {
   const { pokemon } = props;
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
   const goToPokemon = () => {
     console.log("vamos al pokemon: " + pokemon.name);
   };
@@ -16,11 +20,11 @@ export default function PokemonCard(props) {
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
+          <View style={bgStyles}>
             <Text style={styles.number}>
               #{`${pokemon.order}`.padStart(3, 0)}{" "}
             </Text>
-            <Text style={styles.name}> {pokemon.name}</Text>
+            <Text style={styles.name}> {capitalize(pokemon.name)}</Text>
             <Image source={{ uri: pokemon.image }} style={styles.image}></Image>
           </View>
         </View>
@@ -38,7 +42,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: "orange",
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
   },
   number: {
     position: "absolute",
